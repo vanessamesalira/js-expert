b9b21edeaec4b45ddf7c949b6cf2e2cdae2746ef
vanessamesalira <vanessa.mesalira@gmail.com>
2026-03-10 21:18:54 +0000

    Lesson 2: stubs

---
+++ 02-stubs/src/service.js
@@ -0,0 +1,16 @@
+// Services são arquivos utilizados para regras ou integrações externas.
+class Service {
+  async makeRequest(url) {
+    return (await fetch(url)).json()
+  }
+
+  async getPlanets(url) {
+    const data = await this.makeRequest(url)
+    return {
+      name: data.name,
+      surfaceWater: data.surface_water,
+      appeardIn: data.films.length
+    }
+  }
+}
+module.exports = Service
