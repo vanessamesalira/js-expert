52787cb8e1ce45bebb7c5f6eb3ca9277fa01a5c8
vanessamesalira <vanessa.mesalira@gmail.com>
2026-03-14 14:22:32 +0000

    adding Modulo-01 folder

---
+++ modulo-01/04-e2e-coverage/src/api.test.js
@@ -0,0 +1,53 @@
+const { describe, it, after, before } = require('mocha')
+const supertest = require('supertest')
+const assert = require('assert')
+describe('API Suite test', () => {
+  let app
+  before((done) => {
+    app = require('./api')
+    app.once('listening', done)
+  })
+
+  after(done => app.close(done))
+
+  describe('/contact:get', () => {
+    it('should request the contact route and return HTTP Status 200', async () => {
+      const response = await supertest(app)
+        .get('/contact')
+        .expect(200)
+
+      assert.strictEqual(response.text, 'contact us page')
+    })
+  })
+
+  describe('/login:post', () => {
+    it('should request the login and return HTTP Status 200', async () => {
+      const response = await supertest(app)
+        .post('/login')
+        .send({ username: "erickwendel", password: "123" })
+        .expect(200)
+
+      assert.strictEqual(response.text, 'Log in succeeded!')
+    })
+
+    it('should request the login and return HTTP Status 401', async () => {
+      const response = await supertest(app)
+        .post('/login')
+        .send({ username: "xuxadasilva", password: "123" })
+        .expect(401)
+
+      assert.ok(response.unauthorized)
+      assert.strictEqual(response.text, 'Log in failed!')
+    })
+  })
+
+  describe('/hi:get - 404', () => {
+    it('should request and existing page and return HTTP Status 404', async () => {
+      const response = await supertest(app)
+        .get('/hi')
+        .expect(404)
+
+      assert.strictEqual(response.text, 'not found!')
+    })
+  })
+})
